12f41ff82e7693c36b5c47a1b1ce7d6c3cb76dd2
olerie <71316118+olerie@users.noreply.github.com>
2020-09-15 14:37:45 +0200

    Create LED_Lauflicht.ino

---
+++ LED_Lauflicht/LED_Lauflicht.ino
@@ -0,0 +1,39 @@
+/*
+ *Funktion: Vier LED´s sollen als Lauflicht programmiert werden.
+ *Programierer: Ole Riehemann
+ *letzte Änderung: 15.09.2020
+ *Version: 0.0.1
+ *
+ *Hardware: 4x Gruene LED; 1x 220Ohm Widerstand
+ *
+ */
+ 
+ #define grueneLED1 2
+ #define grueneLED2 3
+ #define grueneLED3 4
+ #define grueneLED4 5 
+ void setup()
+{
+  pinMode(grueneLED1, OUTPUT);
+  pinMode(grueneLED2, OUTPUT);
+  pinMode(grueneLED3, OUTPUT);
+  pinMode(grueneLED4, OUTPUT);
+
+}
+
+void loop()
+{
+ digitalWrite(grueneLED1, HIGH);
+ delay(100);
+ digitalWrite(grueneLED1, LOW);
+ digitalWrite(grueneLED2, HIGH);
+ delay(100);
+ digitalWrite(grueneLED2, LOW);
+ digitalWrite(grueneLED3, HIGH);
+ delay(100);
+ digitalWrite(grueneLED3, LOW);
+ digitalWrite(grueneLED4, HIGH);
+ delay(100);
+ digitalWrite(grueneLED4, LOW);
+
+}
