e776394e6fb25324f74ffe3ab109aa45e2f55015
olerie <71316118+olerie@users.noreply.github.com>
2020-09-24 14:58:53 +0200

    Update LED_Lauflicht.ino
    For Schleife Eingefügt

--- LED_Lauflicht/LED_Lauflicht.ino
+++ LED_Lauflicht/LED_Lauflicht.ino
@@ -1,39 +1,43 @@
 /*
- *Funktion: Vier LED´s sollen als Lauflicht programmiert werden.
- *Programierer: Ole Riehemann
- *letzte Änderung: 15.09.2020
- *Version: 0.0.1
- *
- *Hardware: 4x Gruene LED; 1x 220Ohm Widerstand
- *
- */
- 
- #define grueneLED1 2           //Ausgänge werden definiert
- #define grueneLED2 3
- #define grueneLED3 4
- #define grueneLED4 5 
- void setup()
+  Funktion: Vier LED´s sollen als Lauflicht programmiert werden.
+  Programierer: Ole Riehemann
+  letzte Änderung: 15.09.2020
+  Version: 0.0.1
+
+  Hardware: 4x Gruene LED; 1x 220Ohm Widerstand
+
+*/
+
+byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+byte ledZustand [8][8] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0},
+  {1, 1, 0, 0, 0, 0, 0, 0},
+  {1, 1, 1, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 0},
+};
+void setup()
 {
-  pinMode(grueneLED1, OUTPUT);  
-  pinMode(grueneLED2, OUTPUT);
-  pinMode(grueneLED3, OUTPUT);
-  pinMode(grueneLED4, OUTPUT);
+  for (byte i = 0; i < 8; i++)
+  {
+    pinMode(led[i], OUTPUT);
+  }
 
 }
 
 void loop()
 {
- digitalWrite(grueneLED1, HIGH); //Lauflicht startet mit LED 1
- delay(100);
- digitalWrite(grueneLED1, LOW);
- digitalWrite(grueneLED2, HIGH);
- delay(100);
- digitalWrite(grueneLED2, LOW);
- digitalWrite(grueneLED3, HIGH);
- delay(100);
- digitalWrite(grueneLED3, LOW);
- digitalWrite(grueneLED4, HIGH);
- delay(100);
- digitalWrite(grueneLED4, LOW); //Lauflicht endet mit LED 4 und startet wieder mit LED 1
+  for (byte i = 0; i <8; i++) //Zeilen
+  {
+    for (byte j = 0; j < 8; j++)  //Spalten
+    {
+      digitalWrite(led[j], ledZustand[i][j]);
+    }
+    delay(120);
+  }
+
 
 }
