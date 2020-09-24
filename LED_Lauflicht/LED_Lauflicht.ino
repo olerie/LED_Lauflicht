31a21836ec526470e1de05c9a08ee063c4d26e1a
olerie <71316118+olerie@users.noreply.github.com>
2020-09-24 15:02:41 +0200

    Update LED_Lauflicht.ino
    Beschreibung eingefügt

--- LED_Lauflicht/LED_Lauflicht.ino
+++ LED_Lauflicht/LED_Lauflicht.ino
@@ -1,14 +1,14 @@
 /*
-  Funktion: Vier LED´s sollen als Lauflicht programmiert werden.
+  Funktion: 8 LEDs sollen wie KnightRider blinken
   Programierer: Ole Riehemann
-  letzte Änderung: 15.09.2020
+  letzte Änderung: 24.09.2020
   Version: 0.0.1
 
-  Hardware: 4x Gruene LED; 1x 220Ohm Widerstand
+  Hardware: 8x Gruene LED; 8x 220Ohm Widerstand
 
 */
 
-byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9}; // definierung für 2D for Schleife
 byte ledZustand [8][8] = {
   {0, 0, 0, 0, 0, 0, 0, 0},
   {1, 0, 0, 0, 0, 0, 0, 0},
@@ -30,13 +30,13 @@ void setup()
 
 void loop()
 {
-  for (byte i = 0; i <8; i++) //Zeilen
+  for (byte i = 0; i < 8; i++) //Zeilen           // Äußere For Schleife
   {
-    for (byte j = 0; j < 8; j++)  //Spalten
+    for (byte j = 0; j < 8; j++)  //Spalten       // Innere For Schleife
     {
       digitalWrite(led[j], ledZustand[i][j]);
     }
-    delay(120);
+    delay(120);                                   // Verzögerung der Einzelnen LEDs
   }
 
 
